1e130779e4d2ad15ea6bd1b7e7f4941eee27f3c3
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-12-18 10:10:34 +0200

    Fix bug when using storage

--- src/storage.js
+++ src/storage.js
@@ -1,84 +1,101 @@
-const storageManager = (function(){
-    const getDeserialized = (function(){
-        const tasks = localStorage.tasks ? JSON.parse(localStorage.getItem("tasks")) : [];
-        const projects = localStorage.projects ? JSON.parse(localStorage.getItem("projects")) : [];
-        const activeProjectID = localStorage.activeProjectID ? JSON.parse(localStorage.getItem("activeProjectID")) : null;
+const storageManager = (function () {
+  const getDeserialized = (function () {
+    return {
+      get tasks() {
+        return localStorage.tasks
+          ? JSON.parse(localStorage.getItem("tasks"))
+          : [];
+      },
+      get projects() {
+        return localStorage.projects
+          ? JSON.parse(localStorage.getItem("projects"))
+          : [];
+      },
+      get activeProjectID() {
+        return localStorage.activeProjectID
+          ? JSON.parse(localStorage.getItem("activeProjectID"))
+          : null;
+      },
+    };
+  })();
 
-        return{
-            tasks,
-            projects,
-            activeProjectID,
-        }
-    })();
+  function addProject(project) {
+    let storedArr = getDeserialized.projects;
+    storedArr.push(project);
+    localStorage.setItem("projects", JSON.stringify(storedArr));
+  }
 
-    function addProject(project){
-        let storedArr = getDeserialized.projects;
-        storedArr.push(project)
-        localStorage.setItem("projects", JSON.stringify(storedArr));
+  function editProject(editedProject) {
+    let storedArr = getDeserialized.projects;
+    let projectIndex = storedArr.findIndex(
+      (project) => project.uniqueID === editedProject.uniqueID
+    );
+
+    storedArr[projectIndex] = editedProject; // replace with edited Project
+    localStorage.setItem("projects", JSON.stringify(storedArr));
+  }
+
+  function removeProject(project) {
+    let storedArr = getDeserialized.projects;
+    storedArr.splice(
+      storedArr.findIndex((p) => p.uniqueID === project.uniqueID),
+      1
+    );
+    localStorage.setItem("projects", JSON.stringify(storedArr));
+
+    // also remove all tasks of that project
+    if (localStorage.tasks !== undefined) {
+      let storedTasks = getDeserialized.tasks;
+
+      if (storedTasks.length > 0) {
+        let filteredTasks = storedTasks.filter(
+          (task) => task.project.uniqueID !== project.uniqueID
+        );
+        localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+      }
     }
 
-    function editProject(editedProject){
-        let storedArr = getDeserialized.projects;
-        let projectIndex = storedArr.findIndex(project => project.uniqueID === editedProject.uniqueID);
+    // if project was active and remove it from active
+    if (getDeserialized.activeProjectID === project.uniqueID)
+      localStorage.removeItem("activeProjectID");
+  }
 
-        storedArr[projectIndex] = editedProject; // replace with edited Project
-        localStorage.setItem("projects", JSON.stringify(storedArr));
-    }
+  function addTask(task) {
+    let storedArr = getDeserialized.tasks;
+    storedArr.push(task);
+    localStorage.setItem("tasks", JSON.stringify(storedArr));
+  }
 
-    function removeProject(project){
-        let storedArr = getDeserialized.projects;
-        storedArr.splice(storedArr.findIndex(p => p.uniqueID === project.uniqueID), 1);
-        localStorage.setItem("projects", JSON.stringify(storedArr));
+  function editTask(editedTask) {
+    let storedArr = getDeserialized.tasks;
+    let taskIndex = storedArr.findIndex(
+      (task) => task.uniqueID === editedTask.uniqueID
+    );
 
-        // also remove all tasks of that project
-        if(localStorage.tasks !== undefined){
-            let storedTasks = getDeserialized.tasks;
+    storedArr[taskIndex] = editedTask; // replace with edited Project
+    localStorage.setItem("tasks", JSON.stringify(storedArr));
+  }
 
-            if(storedTasks.length > 0){
-                let filteredTasks = storedTasks.filter(task => task.project.uniqueID !== project.uniqueID);
-                localStorage.setItem("tasks", JSON.stringify(filteredTasks));
-            }
-        }
+  function removeTask(task) {
+    let storedArr = getDeserialized.tasks;
+    storedArr.splice(storedArr.indexOf(task), 1);
+    localStorage.setItem("tasks", JSON.stringify(storedArr));
+  }
 
-        // if project was active and remove it from active
-        if(getDeserialized.activeProjectID === project.uniqueID)
-            localStorage.removeItem("activeProjectID");
-    }
+  function saveActiveProjectID(project) {
+    localStorage.setItem("activeProjectID", JSON.stringify(project.uniqueID));
+  }
 
-    function addTask(task){
-        let storedArr = getDeserialized.tasks;
-        storedArr.push(task)
-        localStorage.setItem("tasks", JSON.stringify(storedArr));
-    }
-
-    function editTask(editedTask){
-        let storedArr = getDeserialized.tasks;
-        let taskIndex = storedArr.findIndex(task => task.uniqueID === editedTask.uniqueID);
-
-        storedArr[taskIndex] = editedTask; // replace with edited Project
-        localStorage.setItem("tasks", JSON.stringify(storedArr));
-    }
-
-    function removeTask(task){
-        let storedArr = getDeserialized.tasks;
-        storedArr.splice(storedArr.indexOf(task), 1);
-        localStorage.setItem("tasks", JSON.stringify(storedArr));
-    }
-
-    function saveActiveProjectID(project){
-        localStorage.setItem("activeProjectID", JSON.stringify(project.uniqueID));
-    }
-
-    return{
-        addProject,
-        editProject,
-        removeProject,
-        addTask,
-        editTask,
-        removeTask,
-        saveActiveProjectID,
-        getDeserialized,
-    }
+  return {
+    addProject,
+    editProject,
+    removeProject,
+    addTask,
+    editTask,
+    removeTask,
+    saveActiveProjectID,
+    getDeserialized,
+  };
 })();
 
 export default storageManager;
